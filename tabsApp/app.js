5a77142e20a8ef0e47cb90599e05000cc1cd0903
Michael <GrantM1@users.noreply.github.com>
2020-07-20 19:04:26 +0200

    tabMenu small app, active class + data-id setup

---
+++ tabsApp/app.js
@@ -0,0 +1,20 @@
+const btns = document.querySelectorAll('.tab-btn');
+const about = document.querySelector('.about');
+const articles = document.querySelectorAll('.content');
+
+about.addEventListener('click', function (e) {
+ const id = e.target.dataset.id;
+ // remove actiove from other buttons
+ if (id) {
+  btns.forEach((btn) => {
+   btn.classList.remove('active');
+   e.target.classList.add('active');
+  });
+  // gide other articles and display the mathing id
+  articles.forEach((article) => {
+   article.classList.remove('active');
+  });
+  const element = document.getElementById(id);
+  element.classList.add('active');
+ }
+});
