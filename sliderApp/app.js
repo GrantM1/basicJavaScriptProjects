b819d9bcf7ca52e992cfa8824c1c702f377f10e7
Michael <GrantM1@users.noreply.github.com>
2020-07-31 16:51:53 +0200

    simple slider solution with carousel option or hide tne buttons function

---
+++ sliderApp/app.js
@@ -0,0 +1,39 @@
+const slides = document.querySelectorAll('.slide');
+const nextBtn = document.querySelector('.nextBtn');
+const prevBtn = document.querySelector('.prevBtn');
+
+slides.forEach(function (slide, index) {
+ slide.style.left = `${index * 100}%`;
+});
+
+let counter = 0;
+nextBtn.addEventListener('click', function () {
+ counter++;
+ carousel();
+});
+prevBtn.addEventListener('click', function () {
+ counter--;
+ carousel();
+});
+
+function carousel() {
+ // working with slides
+ // if (counter === slides.length) {
+ //  counter = 0;
+ // }
+ // if (counter < 0) {
+ //  counter = slides.length - 1;
+ // }
+
+ // working with buttons
+ counter < slides.length - 1
+  ? (nextBtn.style.display = 'block')
+  : (nextBtn.style.display = 'none');
+ counter > 0
+  ? (prevBtn.style.display = 'block')
+  : (prevBtn.style.display = 'none');
+ slides.forEach(function (slide) {
+  slide.style.transform = `translateX(-${counter * 100}%)`;
+ });
+}
+prevBtn.style.display = 'none';
